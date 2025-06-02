73e29a35ae0a20e592e2af41a54e5577a7f27ce5
mohamed2000 <husseinmohamed202200@gmail.com>
2025-06-02 17:25:43 -0400

    changing start scripts

--- backend/src/index.js
+++ backend/src/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const userRoutes = require('./routes/userRoutes');
 const adminRoutes = require('./routes/adminRoutes');
+const path = require('path');
 
 const app = express();
 const PORT = 3001;
@@ -13,6 +14,14 @@ app.use(express.json());
 // Routes
 app.use('/api/users', userRoutes);
 app.use('/api/admin', adminRoutes);
+
+// Serve React build static files in production
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '../../client/build')));
+  app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '../../client/build', 'index.html'));
+  });
+}
 
 // Error handling middleware
 app.use((err, req, res, next) => {
