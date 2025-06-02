33c6d490f119a65c68f333200f05e7c026799adc
mohamed2000 <husseinmohamed202200@gmail.com>
2025-06-02 17:03:01 -0400

    Fixing backend to 3001

--- backend/src/index.js
+++ backend/src/index.js
@@ -4,7 +4,7 @@ const userRoutes = require('./routes/userRoutes');
 const adminRoutes = require('./routes/adminRoutes');
 
 const app = express();
-const PORT = process.env.PORT || 3001;
+const PORT = 3001;
 
 // Middleware
 app.use(cors());
